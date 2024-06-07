65acc71dfc8173c602d425a968bc1fcfa6297106
Ari Álvarez <70560259+ariigat0@users.noreply.github.com>
2024-06-06 18:23:16 -0600

    Update translator.py

--- JoE_Compiladores/translator.py
+++ JoE_Compiladores/translator.py
@@ -524,38 +524,19 @@ while True:
    '''
 
 # Image Segmentation with Watershed Algorithm
+# Cargar la imagen
 img = cv2.imread('coins.jpeg')
-assert img is not None, "file could not be read, check with os.path.exists()"
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-# noise removal
-kernel = np.ones((3,3),np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
- 
-# sure background area
-sure_bg = cv2.dilate(opening,kernel,iterations=3)
- 
-# Finding sure foreground area
-dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
- 
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
-unknown = cv2.subtract(sure_bg,sure_fg)
+# Asegurarse de que la imagen se cargue correctamente
+assert img is not None, "El archivo no pudo ser leído, verifica con os.path.exists()"
 
-# Marker labelling
-ret, markers = cv2.connectedComponents(sure_fg)
- 
-# Add one to all labels so that sure background is not 0, but 1
-markers = markers+1
- 
-# Now, mark the region of unknown with zero
-markers[unknown==255] = 0
+# Aplicar la segmentación de Watershed
+segmented_img = segment_watershed(img)
 
-markers = cv2.watershed(img,markers)
-img[markers == -1] = [255,0,0]
+# Mostrar la imagen segmentada
+cv2.imshow('Imagen Segmentada', segmented_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 print("\nended")
 
-# from networkx.drawing.nx_pydot import graphviz_layout
